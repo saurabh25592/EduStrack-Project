a16e684c00803fe11740d6951985fbd5a3128a25
Saurabh pachpute <saurabhpachpute25@gmail.com>
2025-02-15 17:53:17 +0530

    Add files via upload

---
+++ EduStrack/src/main/java/com/tka/sams/api/EdustrackApplication.java
@@ -0,0 +1,13 @@
+package com.tka.sams.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdustrackApplication {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(EdustrackApplication.class, args);
+		System.out.println("Spring boot started.....");	    
+	}
+}
---
+++ EduStrack/src/main/java/com/tka/sams/api/entity/Student.java
@@ -0,0 +1,51 @@
+package com.tka.sams.api.entity;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+
+@Entity
+public class Student {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String email;
+
+	public Student() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+}
